3995601ef194332d4207b6c36e389257c8899bad
alexortner <alex.von.tingel@gmail.com>
2022-11-10 09:15:41 +0100

    frontend dashboard

---
+++ 1_setup/7_frontends/1_dashboard/1_code/src/components/Dashboard.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="mb-3">
+        <Card
+          title="Jupyter"
+          text="Notebook for Python & PySpark"
+          url_label="Open Jupyter"
+          url="https://test.de"
+          imgURL="../../assets/jupyter_logo.png"
+        ></Card
+      ></v-col>
+      <v-col cols="mb-3">
+        <Card
+          title="Zeppelin"
+          text="Notebook for Spark (Scala, Python, SQL)"
+          url_label="Open Zeppelin"
+          url="https://test.de"
+          imgURL="../assets/jupyter_logo.png"
+        ></Card
+      ></v-col>
+    </v-row>
+    <v-row> </v-row>
+  </v-container>
+</template>
+
+<script>
+import Card from "@/components/Card";
+export default {
+  name: "Dashboard",
+  components: {
+    Card,
+  },
+
+  data: () => ({}),
+};
+</script>

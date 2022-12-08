55fb9e5b1f9f0eed71f0d7888cfa0d28d5f34d37
Chris Ulpinnis <culpinnis@gmail.com>
2022-12-08 15:57:54 +0100

    Added helper scripts

---
+++ 1_setup/1_terraform/terraform_plan.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+IFS=$'\n'       # make newlines the only separator
+set -f          # disable globbing
+pwd=$PWD
+
+for i in $(cat < "$1"); do
+  echo "terraform workspace select $i"
+  cd "$pwd/aks" && terraform workspace select $i && terraform plan -out ${i}.plan 2>&1 | tee "${i}_plan.log"
+  sleep 10
+done
